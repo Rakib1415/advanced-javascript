5eb4b9721025cb5356281c9c5ed3c7bde5e61a35
Rakib1415 <rakibiuaece@gmail.com>
2020-03-04 10:23:14 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,26 @@
+// any number is true without zero number 
+
+// any string is true without emty string 
+
+// Falsy value 
+// undefined
+// null 
+// NaN 
+// 0
+// ""
+// false 
+
+// Truthy value 
+// {}
+// []
+// " "
+
+
+const name = "0";
+
+if(name){
+    console.log("Condition is True");
+}
+else{
+    console.log("Condition is False");
+}
