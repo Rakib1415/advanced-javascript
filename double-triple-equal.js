f6d7dc7cc5b21ef93d3266a01cfcde16aad6e17c
Rakib1415 <rakibiuaece@gmail.com>
2020-03-04 12:25:25 +0600

    double-triple-equal

---
+++ double-triple-equal.js
@@ -0,0 +1,14 @@
+const first = 1;
+const second = false;
+
+if(first === second){
+    console.log("Condition is True");
+}
+else {
+    console.log("Condition is False");
+}
+
+// 1 = true 
+// 0 = false 
+// === is check value and data type
+// == is check only value 
