2a9240ce745bc4b232e8ca9291c2af304b745ff9
Rakib1415 <rakibiuaece@gmail.com>
2020-03-04 12:10:47 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+
+let name;
+
+console.log(name);
+
+function add(num1,num2){
+    console.log(num1 + num2);
+    return;
+}
+   function add(num1, num2){
+       console.log(num1,num2);
+   }
+
+const person = {name:"sakib", age:12};
+
+console.log(person.gf);
+
+let fun = undefined;
+console.log(fun);
+
+// null is not excistance or khali or nai
+
+const ages = [1,2,3];
+
+console.log(ages[11]);
